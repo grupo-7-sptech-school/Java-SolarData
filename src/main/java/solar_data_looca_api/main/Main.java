311655df837120d783318ee14e0740f066f41dcd
artur-al <arturmcs3@gmail.com>
2025-11-04 23:03:02 -0300

    upp: looping em tempo de execução

--- src/main/java/solar_data_looca_api/main/Main.java
+++ src/main/java/solar_data_looca_api/main/Main.java
@@ -5,20 +5,65 @@ import solar_data_looca_api.group.memoria.Memoria;
 import solar_data_looca_api.group.processador.Processador;
 import solar_data_looca_api.group.processos.Processo;
 import solar_data_looca_api.group.processos.ProcessoGrupo;
+
 import java.sql.*;
 import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main {
-    public static void main(String[] args) throws SQLException, UnknownHostException {
 
+    // Variaveis globais para evitar recriacao
+    private static String hostname;
+    private static int hostNumerico;
+    private static String macAddress;
+    private static String ip;
+    private static int idEmpresa;
+    private static boolean initialized = false;
+    private static final AtomicInteger executionCount = new AtomicInteger(0);
+
+    public static void main(String[] args) {
+
+        while (true) {
+            try {
+                executarColeta();
+
+                Thread.sleep(15000);
+
+            } catch (Exception e) {
+                System.err.println("Erro na execucao: " + e.getMessage());
+                e.printStackTrace();
+
+                try {
+                    Thread.sleep(60000);
+                } catch (InterruptedException ie) {
+                    Thread.currentThread().interrupt();
+                    break;
+                }
+            }
+        }
+    }
+
+    private static void executarColeta() throws SQLException, UnknownHostException, SocketException {
         String Banco = "jdbc:mysql://34.198.76.254:3306/solarData01";
         String user = "solardata";
         String password = "Solar@Data01";
 
         try (Connection connection = DriverManager.getConnection(Banco, user, password)) {
+            int execucaoAtual = executionCount.incrementAndGet();
+            System.out.println("\n-- Execução " + execucaoAtual + " --");
             System.out.println("Conectado ao banco de dados");
+
+            if (!initialized) {
+                inicializarInformacoesMaquina();
+                idEmpresa = obterOuCriarEmpresa(connection);
+                inserirMaquinaSeNecessario(connection);
+                initialized = true;
+            }
 
             Looca looca = new Looca();
             Processador proc1 = new Processador();
@@ -26,38 +71,157 @@ public class Main {
             ProcessoGrupo processoGrupo = new ProcessoGrupo();
             List<Processo> processos = processoGrupo.getProcessos();
 
-            String hostname = InetAddress.getLocalHost().getHostName();
-
-            inserirHostnameSeNecessario(connection, hostname);
-
             ordenarProcessos(processos);
-            inserirProcessos(connection, processos, proc1, mem1, hostname);
+            inserirProcessos(connection, processos, proc1, mem1);
+
+            System.out.println("Execucao " + execucaoAtual + " completada");
         }
     }
 
-    private static void inserirHostnameSeNecessario(Connection connection, String hostname) throws SQLException {
-        String verificarSql = "SELECT COUNT(*) FROM Maquina WHERE hostname = ?";
-        try (PreparedStatement verificarStmt = connection.prepareStatement(verificarSql)) {
-            verificarStmt.setString(1, hostname);
-            ResultSet rs = verificarStmt.executeQuery();
+    private static void inicializarInformacoesMaquina() throws UnknownHostException, SocketException {
+        hostname = InetAddress.getLocalHost().getHostName();
+        hostNumerico = hostname.hashCode() & 0x7FFFFFFF;
+        macAddress = obterMacAddress();
+        ip = obterIp();
 
-            if (rs.next() && rs.getInt(1) == 0) {
-                String inserirSql = "INSERT INTO Maquina (hostname) VALUES (?)";
-                try (PreparedStatement inserirStmt = connection.prepareStatement(inserirSql)) {
-                    inserirStmt.setString(1, hostname);
-                    inserirStmt.executeUpdate();
-                    System.out.println("Hostname inserido: " + hostname);
-                }
+        System.out.println("Maquina: " + hostname + " (ID: " + hostNumerico + ")");
+        System.out.println("MAC: " + macAddress + ", IP: " + ip);
+    }
+
+    private static int obterOuCriarEmpresa(Connection connection) throws SQLException {
+
+        String verificarEmpresaSql = "SELECT idEmpresa FROM Empresa LIMIT 1";
+        try (PreparedStatement stmt = connection.prepareStatement(verificarEmpresaSql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            if (rs.next()) {
+                int empresaId = rs.getInt("idEmpresa");
+                System.out.println("Empresa encontrada: ID " + empresaId);
+                return empresaId;
             } else {
-                System.out.println("Hostname já existe no banco: " + hostname);
+                System.out.println("Criando empresa padrao...");
+                return criarEmpresaAutomatica(connection);
             }
+        }
+    }
+
+    private static int criarEmpresaAutomatica(Connection connection) throws SQLException {
+
+        int idEndereco = criarEnderecoPadrao(connection);
+
+
+        String sql = "INSERT INTO Empresa (fkEndereco, razaoSocial, nomeFantasia, cnpj) VALUES (?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            String nomeEmpresa = "Empresa - " + hostname;
+            String cnpj = gerarCnpjUnico();
+
+            stmt.setInt(1, idEndereco);
+            stmt.setString(2, nomeEmpresa);
+            stmt.setString(3, nomeEmpresa);
+            stmt.setString(4, cnpj);
+            stmt.executeUpdate();
+
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    int empresaId = rs.getInt(1);
+                    System.out.println("Empresa criada: " + nomeEmpresa + " (ID: " + empresaId + ")");
+                    return empresaId;
+                }
+            }
+        }
+        throw new SQLException("Falha ao criar empresa automatica");
+    }
+
+    private static int criarEnderecoPadrao(Connection connection) throws SQLException {
+        String sql = "INSERT INTO Endereco (cep, logradouro, numero, bairro, cidade, estado, complemento) VALUES (?, ?, ?, ?, ?, ?, ?)";
+        try (PreparedStatement stmt = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            stmt.setString(1, "00000000");
+            stmt.setString(2, "Endereco Automatico");
+            stmt.setString(3, "0");
+            stmt.setString(4, "Centro");
+            stmt.setString(5, "Sao Paulo");
+            stmt.setString(6, "SP");
+            stmt.setString(7, "Criado automaticamente");
+            stmt.executeUpdate();
+
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+            }
+        }
+        throw new SQLException("Falha ao criar endereco padrao");
+    }
+
+    private static String gerarCnpjUnico() {
+        long timestamp = System.currentTimeMillis();
+        int random = (int) (Math.random() * 10000);
+        return String.format("%014d", (timestamp + random) % 100000000000000L);
+    }
+
+    private static void inserirMaquinaSeNecessario(Connection connection) throws SQLException {
+        String verificarSql = "SELECT COUNT(*) FROM Maquina WHERE hostName = ?";
+        try (PreparedStatement verificarStmt = connection.prepareStatement(verificarSql)) {
+            verificarStmt.setInt(1, hostNumerico);
+            try (ResultSet rs = verificarStmt.executeQuery()) {
+                if (rs.next() && rs.getInt(1) == 0) {
+                    String inserirSql = "INSERT INTO Maquina (hostName, identificador, fkEmpresa, macAdress, ip) VALUES (?, ?, ?, ?, ?)";
+                    try (PreparedStatement inserirStmt = connection.prepareStatement(inserirSql)) {
+                        inserirStmt.setInt(1, hostNumerico);
+                        inserirStmt.setString(2, hostname);
+                        inserirStmt.setInt(3, idEmpresa);
+                        inserirStmt.setString(4, macAddress);
+                        inserirStmt.setString(5, ip);
+                        inserirStmt.executeUpdate();
+                        System.out.println("Maquina inserida no banco");
+                    }
+                } else {
+                    System.out.println("Maquina ja existe no banco");
+                }
+            }
+        }
+    }
+
+    private static String obterMacAddress() throws SocketException {
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        while (networkInterfaces.hasMoreElements()) {
+            NetworkInterface networkInterface = networkInterfaces.nextElement();
+            byte[] mac = networkInterface.getHardwareAddress();
+            if (mac != null && !networkInterface.isLoopback() && networkInterface.isUp()) {
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < mac.length; i++) {
+                    sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? ":" : ""));
+                }
+                return sb.toString();
+            }
+        }
+        return "00:00:00:00:00:00";
+    }
+
+    private static String obterIp() throws UnknownHostException, SocketException {
+        try {
+            return InetAddress.getLocalHost().getHostAddress();
+        } catch (Exception e) {
+            Enumeration<NetworkInterface> interfaces = NetworkInterface.getNetworkInterfaces();
+            while (interfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = interfaces.nextElement();
+                if (networkInterface.isLoopback() || !networkInterface.isUp()) continue;
+
+                Enumeration<InetAddress> addresses = networkInterface.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    InetAddress address = addresses.nextElement();
+                    if (address.isSiteLocalAddress()) {
+                        return address.getHostAddress();
+                    }
+                }
+            }
+            return "127.0.0.1";
         }
     }
 
     private static void ordenarProcessos(List<Processo> processos) {
         for (int i = 0; i < processos.size() - 1; i++) {
             int maiorValor = i;
-
             for (int j = i + 1; j < processos.size(); j++) {
                 if (processos.get(j).getUsoCpu() > processos.get(maiorValor).getUsoCpu()) {
                     maiorValor = j;
@@ -71,40 +235,34 @@ public class Main {
         }
     }
 
-    private static void inserirProcessos(Connection connection, List<Processo> processos, Processador proc1, Memoria mem1, String hostname) throws SQLException {
-        System.out.println("Hostname: " + hostname);
+    private static void inserirProcessos(Connection connection, List<Processo> processos, Processador proc1, Memoria mem1) throws SQLException {
+        System.out.println("Coletando dados de " + processos.size() + " processos...");
 
+        int processosInseridos = 0;
         for (int i = 0; i < Math.min(100, processos.size()); i++) {
             Processo p = processos.get(i);
 
-            PreparedStatement conexaoBD = connection.prepareStatement(
-                    "INSERT INTO Processo (pid, nome, cpuPorcentagem, ramPorcentagem, fkMaquina, tipo) VALUES (?, ?, ?, ?, ?, ?)");
+            String sql = "INSERT INTO Processo (pid, nome, cpuPorcentagem, ramPorcentagem, fkMaquina, tipo) VALUES (?, ?, ?, ?, ?, ?)";
+            try (PreparedStatement stmt = connection.prepareStatement(sql)) {
+                Integer totalNucleos = proc1.getNumeroCpusLogicas();
+                double memoriaGB = mem1.getTotal() / (1024.0 * 1024.0 * 1024.0);
+                double ramGB = p.getUsoMemoria();
+                double porcentagemRAM = (ramGB / memoriaGB) * 100.0;
+                double porcentagemCPU = p.getUsoCpu() / totalNucleos;
 
-            Integer totalNucleos = proc1.getNumeroCpusLogicas();
-            double memoriaGB = mem1.getTotal() / (1024.0 * 1024.0 * 1024.0);
-            double ramGB = p.getUsoMemoria();
-            double porcentagemRAM = (ramGB / memoriaGB) * 100.0;
-            double porcentagemCPU = p.getUsoCpu() / totalNucleos;
+                String tipo = (porcentagemCPU > 1. || porcentagemRAM > 5.) ? "QUENTE" : "FRIO";
 
-            if (porcentagemCPU > 1. || porcentagemRAM > 5.) {
-                conexaoBD.setString(6, "QUENTE");
-            } else {
-                conexaoBD.setString(6, "FRIO");
-            }
+                stmt.setInt(1, p.getPid());
+                stmt.setString(2, p.getNome());
+                stmt.setDouble(3, porcentagemCPU);
+                stmt.setDouble(4, porcentagemRAM);
+                stmt.setInt(5, hostNumerico);
+                stmt.setString(6, tipo);
 
-            conexaoBD.setInt(1, p.getPid());
-            conexaoBD.setString(2, p.getNome());
-            conexaoBD.setDouble(3, porcentagemCPU);
-            conexaoBD.setDouble(4, porcentagemRAM);
-            conexaoBD.setString(5, hostname);
-
-            conexaoBD.executeUpdate();
-            conexaoBD.close();
-
-            if (i == 0) {
-                System.out.println("Inserindo registros, aguarde...");
+                stmt.executeUpdate();
+                processosInseridos++;
             }
         }
-        System.out.println("Registros inseridos com sucesso!");
+        System.out.println(processosInseridos + " processos inseridos");
     }
 }
